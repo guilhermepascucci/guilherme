298e878c9df44e9c06bc73963beef275af20c6ea
guilhermepascucci <99357352+guilhermepascucci@users.noreply.github.com>
2024-04-15 16:17:32 -0300

    Update codigoThreads.c

--- Projeto 1/codigoThreads.c
+++ Projeto 1/codigoThreads.c
@@ -18,37 +18,54 @@ void *use_escalator(void *arg) {
     Person *p = (Person *)arg;
 
     pthread_mutex_lock(&lock);
+    // Se a escada estiver parada ou se movendo na mesma direção.
     if (esc_direction == -1 || esc_direction == p->direction) {
-        if (last_time < p->arrival_time) {
-            last_time = p->arrival_time;
-        }
-        last_time += 10;
-        esc_direction = p->direction;
-    } else {
-        if (last_time < p->arrival_time) {
-            last_time = p->arrival_time;
+        // Se a escada estiver parada ou se a próxima pessoa chegar após o último uso.
+        if (last_time <= p->arrival_time) {
+            last_time = p->arrival_time + 10; // Começa e atravessa
         } else {
-            last_time += 10; // Espera a escada parar
+            last_time += 10; // Já estava em movimento, apenas atravessa
         }
-        last_time += 10; // Tempo para atravessar
-        esc_direction = p->direction;
+    } else {
+        // Escada está se movendo na direção oposta.
+        // Se a próxima pessoa chegar após a escada ter parado e mudado de direção.
+        if (last_time < p->arrival_time) {
+            last_time = p->arrival_time; // A escada já estaria parada quando a pessoa chega
+        }
+        // Adiciona tempo para a escada parar se necessário e para atravessar.
+        last_time += 20;
     }
+    esc_direction = p->direction; // Atualiza a direção da escada
     pthread_mutex_unlock(&lock);
 
     pthread_exit(NULL);
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+    // argc é a contagem de argumentos, argv é o array de argumentos
+    if (argc != 2) {
+        fprintf(stderr, "input\n", argv[0]);
+        return 1;
+    }
+
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL) {
+        perror("Error opening file");
+        return 1;
+    }
+
     int N;
-    scanf("%d", &N);
+    fscanf(file, "%d", &N);
 
     pthread_t threads[N];
     Person people[N];
     pthread_mutex_init(&lock, NULL);
 
     for (int i = 0; i < N; i++) {
-        scanf("%d %d", &people[i].arrival_time, &people[i].direction);
+        fscanf(file, "%d %d", &people[i].arrival_time, &people[i].direction);
     }
+
+    fclose(file); // Fechar o arquivo após ler os dados
 
     // Criar uma thread para cada pessoa
     for (int i = 0; i < N; i++) {
